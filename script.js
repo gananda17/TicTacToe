c5c6ed02c7e9fea95d5f5546b37083508cf1e598
gananda17 <44800600+gananda17@users.noreply.github.com>
2019-05-02 11:20:17 +0700

    Test

---
+++ script.js
@@ -0,0 +1,19 @@
+function Game() {
+  this.gameOn = true;
+  this.history = [[],[]];
+  this.clicked = (tile, player) => {
+      this.history[player].push(tile);
+      this.checkWinner(player);
+  };
+  this.checkWinner = (player) => {
+   x = this.history[player];
+      if (x.includes(1) && x.includes(2) && x.includes(3) || x.includes(1) && x.includes(5) && x.includes(9) || x.includes(1) && x.includes(4) && x.includes(7) || x.includes(2) && x.includes(5) && x.includes(8) || x.includes(3) && x.includes(5) && x.includes(7) || x.includes(3) && x.includes(6) && x.includes(9) || x.includes(4) && x.includes(5) && x.includes(6) || x.includes(7) && x.includes(8) && x.includes(9)) {
+          this.winner(player);
+          this.gameOn = false;
+      }
+  }
+  this.winner = (player) => {
+  if(player == 0) {alert("winner is X")}else{alert("winner is O");}
+  }
+}
+var newGame = new Game();
